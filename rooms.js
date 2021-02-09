49231a4daa63165ce3dba8629261c3e85cabe0d0
phoenixzero4 <phoenixzero4@hotmail.com>
2021-02-08 19:50:29 -0800

    just getting started

--- rooms.js
+++ rooms.js
@@ -5,7 +5,7 @@ rooms = {
         "objects": [{
                 "obj_name": "Arched Doorway",
                 "obj_description": "The ornately carved archway leads into a Passageway.",
-                "obj_actions": ["LOOK", "ENTER"],
+                "obj_actions": ["LOOK", "ENTER", "BACK"],
                 "obj_destination": "passage"
             },
             {
@@ -23,6 +23,29 @@ rooms = {
             "obj_description": "The ornately carved archway leads into the Entranceway.",
             "obj_actions": ["LOOK", "ENTER"],
             "obj_destination": "entrance"
-        }]
+        }
+      ,{
+        "obj_name": "small door",
+        "obj_description": "The doorway is just big enough to struggle through",
+        "obj_actions": ["LOOK", "ENTER"],
+        "obj_destination": "room1"
+      }
+    ]
+    },
+    "room1": {
+      "name" : "room1",  // TODO add more descriptive room names later
+      "description": "The room has an open doorway on one wall and a very small door on the opposite. A sign is posted next to the open doorway.",
+      "objects": [{
+        "obj_name": "small door",
+        "obj_description": "The doorway is just big enough to struggle through",
+        "obj_actions": ["LOOK", "ENTER"],
+        "obj_destination": "passage"
+      },
+    {
+      "obj_name": "open doorway",
+      "obj_description": "The doorway is open with no door on its hinges. A sign next to it reads 'Welcome Covidiot'. You have been presented with the unique opportunity to look behind the veil of disinformation surrounding Covid-19 and the 'Pandemic'. Find your way out of the maze by solving puzzles and remaining a free-thinker. Your reward? A return to normalcy. Fail and you remain a sheep in 'the New Normal'. Avoid the evil Dr. Fauci, the hideous Dr. Tam, and other minions with power on their minds and deceit on their tongues. We have a small gift to help you get started. So put on your mask, wash your hands are prepare for <h1 style='color:red'; 'align-self:center'>Mandatory Vaccinator</h1>",
+      "obj_actions": ["LOOK", "ENTER", "TAKE"],
+      "obj_destination": "room2" //laboratory
+    }]
     }
 };
--- text.js
+++ text.js
@@ -2,6 +2,7 @@ class Game {
     constructor(location_start, rooms) {
         this.location = location_start;
         this.rooms = rooms;
+        this.inventory = ['sword'];
     }
 
     parse(command) {
@@ -14,6 +15,14 @@ class Game {
                 break;
             }
         }
+        if(target == ""){ 
+          for (var i = 0; i < this.inventory.length; i++) { 
+                if (words.includes(this.inventory[i].obj_name.toUpperCase())) { 
+                    target = this.inventory[i] ;
+                    break; 
+            } 
+          } 
+        } 
         if (target != "") {
             var verbs = target.obj_actions;
             var action = "";
@@ -23,27 +32,60 @@ class Game {
                     break;
                 }
             }
+           
             switch (action) {
+              
                 case "ENTER":
                     this.location = target.obj_destination;
-                    var output = "You enter the " + target.obj_name + " and emerge in the " + this.rooms[target.obj_destination].name + ". ";
+                    var output = "You enter the <strong style='color:#800080'>" + target.obj_name + "</strong> and emerge in the " + this.rooms[target.obj_destination].name + ". ";
                     output += this.rooms[target.obj_destination].description + " ";
                     output += this.listRoomObjects();
+                   
                     break;
                 case "LOOK":
-                    var output = "You look at the " + target.obj_name + ". ";
+                    var output = "You look at the <strong style='color:#2196f3'>" + target.obj_name + "</strong>. ";
                     output += target.obj_description;
+                    last = output;
                     break;
+                case "TAKE": 
+                    for (var i = 0; i < objects.length; i++) { 
+                        if(target == objects[i]){ 
+                          var output = "You take the " + target.obj_name + ". "; 
+                          this.inventory.push(target); 
+                          this.rooms[this.location].objects.splice(1, 1); 
+                          return output; 
+                        } 
+                    } 
                 default:
                     return "You are unsure of what to do."
             }
             return output
-        } else {
+        }
+        if(target == ""){ 
+          var verbs_user = ["INVENTORY"] 
+          for (var i = 0; i < verbs_user.length; i++) { 
+                    if (words.includes(verbs_user[i].toUpperCase())) { 
+                        action = verbs_user[i] 
+                        break; 
+                    } 
+                } 
+          if(action != ""){ 
+            switch (action) { 
+            case "INVENTORY": 
+                        return this.listInventoryObjects(); 
+                        break; 
+                    default: 
+                        return "You are unsure of what to do."; 
+            } 
+          }else{ 
+            return "You are unsure of what to do."; 
+          } 
+        }  else {
             return "You are unsure of what to do."
         }
 
     }
-
+    // lists whatever exists in the room for the player to interact with
     listRoomObjects() {
         var current_location = this.location;
         var rooms = this.rooms;
@@ -60,17 +102,36 @@ class Game {
         }
         return output;
     }
+    listInventoryObjects() { 
+      var output = "Your inventory contains a "; 
+      for (var i = 0; i < this.inventory.length; i++){ 
+        output += this.inventory[i].obj_name; 
+              if (i == this.inventory.length - 2) { 
+                  output += ", and a "; 
+              } else if (i < this.inventory.length - 2) { 
+                  output += ", a "; 
+              } else { 
+                  output += "."; 
+              } 
+      } 
+      return output; 
+    } 
 }
-
+  // kickoff the game
 gameobj = new Game("entrance", rooms)
 document.getElementById("log").innerHTML += "<br>" + "You hear a crash as the staircase behind you collapses. You're trapped in here now.";
 document.getElementById("log").innerHTML += "<br>" + gameobj.listRoomObjects();
 const node = document.getElementById("playerInput");
+
 node.addEventListener('keyup', ({
     key
 }) => {
     if (key === "Enter") {
-        document.getElementById("log").innerHTML += "<br>" + gameobj.parse(node.value);
+        //document.getElementById("log").innerHTML += "<br>"+ gameobj.parse(node.value);
+        var log = document.getElementById('log');
+        var newlog = "<strong>" + gameobj.parse(node.value) + "</strong>" + "<br><p>" + log.innerHTML + "</p>"; 
+        log.innerHTML = "";
+        log.innerHTML += newlog;
         node.value = "";
     }
 })
